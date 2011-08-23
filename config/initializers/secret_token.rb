ff6be7abc96f074bd716dd327fa4883247f9cf61
robin <robin.wu.cn@gmail.com>
2011-08-23 15:48:02 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyui::Application.config.secret_token = '44b8b9fd27b56aebdc68748e64355c03f159d727b30201c227d5350222318534c4b30ee5ccc3b71b7d918bd06ef6a21477636387f846fff76964919bded7878d'
